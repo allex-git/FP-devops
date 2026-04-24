3752172853a7395178e5357990988e155a4c3257
allex-git <allexshop365@gmail.com>
2026-04-24 17:56:41 +0300

    FP add app & dockerfile

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# 1. Минимальный официальный образ
+FROM python:3.12-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY app/requirements.txt .
+
+RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
+
+COPY app/ .
+
+LABEL maintainer="AllexDevOps"
+LABEL project="Final-Project"
+LABEL description="Final Project"
+
+RUN useradd -m appuser && chown -R appuser:appuser /app
+USER appuser
+
+EXPOSE 8080
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
---
+++ app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import socket
+from datetime import datetime, timezone
+
+app = FastAPI()
+
+START_TIME = datetime.now(timezone.utc)
+
+
+def get_host_info():
+    hostname = socket.gethostname()
+    try:
+        ip = socket.gethostbyname(hostname)
+    except Exception:
+        ip = "unknown"
+    return hostname, ip
+
+@app.get("/")
+def root():
+    hostname, ip = get_host_info()
+
+    now = datetime.now(timezone.utc)
+    uptime_seconds = int((now - START_TIME).total_seconds())
+
+    return {
+        "status": "ok",
+        "hostname": hostname,
+        "ip": ip,
+        "author": "Allex_DevOps",
+        "datetime": now.isoformat(),
+        "started_at": START_TIME.isoformat(),
+        "uptime_seconds": uptime_seconds
+    }
+
+
+@app.get("/healthz")
+def health():
+    return {
+        "status": "healthy"
+    }
