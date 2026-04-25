3983c49070a62f09f00c208c03c06e32546cb331
allex-git <allexshop365@gmail.com>
2026-04-25 10:54:34 +0300

    FP fix

--- EKS/eks-worker-nodes.tf
+++ EKS/eks-worker-nodes.tf
@@ -5,8 +5,8 @@ resource "aws_eks_node_group" "danit" {
   subnet_ids = module.vpc.private_subnets
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 2
     min_size     = 1
   }
 
