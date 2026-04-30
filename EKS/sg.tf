33473006e43d29e14e49d303c872dab79f0a033a
allex-git <allexshop365@gmail.com>
2026-04-30 16:25:12 +0300

    fix

--- EKS/sg.tf
+++ EKS/sg.tf
@@ -15,7 +15,14 @@ resource "aws_security_group" "danit-cluster" {
     created_by = var.name
   })
 }
-
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.danit-cluster.id
+}
 resource "aws_security_group_rule" "allow_https" {
   type              = "ingress"
   from_port         = 443
